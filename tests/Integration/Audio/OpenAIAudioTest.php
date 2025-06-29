11f05c6c1584daf37bf0e3f1153f9111ef14e34b
Franco Lombardo <f.lombardo69@gmail.com>
2025-06-29 19:47:42 +0200

    Fix translate API - improve tests (#381)

--- tests/Integration/Audio/OpenAIAudioTest.php
+++ tests/Integration/Audio/OpenAIAudioTest.php
@@ -12,12 +12,19 @@ it('can transcribe audio files', function () {
     $transcription = $audio->transcribe(__DIR__.'/wikipedia.ogg');
     expect($transcription->text)->toBe('Wikipedia, the free encyclopedia.')
         ->and($transcription->language)->toBe('english')
-        ->and($transcription->durationInSeconds)->toBeBetween(2.46, 2.48);
+        ->and($transcription->durationInSeconds)->toBeBetween(2.45, 2.48);
 });
 
 it('can translate audio files', function () {
     $audio = new OpenAIAudio();
     // Original author of the audio file is KenKuhl, clipped by Davidzdh, CC BY-SA 3.0 via Wikimedia Commons
-    $translation = $audio->translate(__DIR__.'/wikipedia.ogg');
-    expect($translation)->toBe('Wikipedia, the free encyclopedia.');
+    $translation = $audio->translate(__DIR__.'/French_Can_I_have_a_table_for_two_please.wav');
+    expect($translation)->toBe('Can I have a table for two, please?');
+});
+
+it('can translate audio files using a prompt', function () {
+    $audio = new OpenAIAudio();
+    // Original author of the audio file is KenKuhl, clipped by Davidzdh, CC BY-SA 3.0 via Wikimedia Commons
+    $translation = $audio->translate(__DIR__.'/French_Can_I_have_a_table_for_two_please.wav', 'This audio is in French');
+    expect($translation)->toBe('Can I have a table for two, please?');
 });
